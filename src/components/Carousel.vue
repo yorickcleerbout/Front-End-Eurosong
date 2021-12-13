6fafa829fcddc8a413f9c8b888f940606f5b4b40
Yorick Cleerbout <yorick.cleerbout@outlook.com>
2021-12-13 10:25:36 +0100

    Added Game and Carousel component for displaying songs

---
+++ src/components/Carousel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div v-for="song in items" :key="song.id">
+            {{ song.artist.name }} - {{ song.title }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Carousel",
+    props: [
+        "items"
+    ]
+}
+</script>
--- src/components/pages/Game.vue
+++ src/components/pages/Game.vue
@@ -2,16 +2,66 @@
     <div>
         <h1>Game</h1>
         <button @click="goToPage('home')">Go To Home</button>
+
+        
+        <Carousel :items="songs"/>
     </div>
 </template>
 
 <script>
+// Components
+import Carousel from '../Carousel.vue';
+
 export default {
     name: "Gamepage",
+    components: {
+        Carousel
+    },
+    data() {
+        return {
+            songs: []
+        }
+    },
+    mounted() {
+        console.log("Mounted and ready to fetch data!");
+        this.fetchSongs();
+        
+        
+
+    },
     methods: {
+        // Navigation
         goToPage(page) {
             this.$emit("change-page", page);
+        },
+        // Data Methods
+        fetchSongs() {
+            const url = "http://webservies.be/eurosong/Songs";
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((songs) => {
+                    this.fetchArtists(songs);
+                });
+        },
+        fetchArtists(songs) {
+            const url = "http://webservies.be/eurosong/Artists";
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((artists) => {
+                    songs.map(song => {
+                       const artist = artists.find((artist) => artist.id == song.artist);
+                       song.artist = artist;
+                       return song;
+                    });
+                    this.songs = songs;
+                });
         }
+
+
     }
 }
 </script>
