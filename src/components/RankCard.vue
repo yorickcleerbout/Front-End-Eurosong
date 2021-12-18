61cde06667447f1458eedd9f136900d4044bf735
yorickcleerbout <70236777+yorickcleerbout@users.noreply.github.com>
2021-12-18 14:07:33 +0100

    Added content for Ranking page

---
+++ src/components/RankCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="card">
+        <div class="imgBx">                  
+            <iframe :src="song.spotify" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </div>
+
+        <div class="contentBx">
+            <h2>{{ song.title }} - {{ song.artist.name }}</h2>
+            <div class="size">
+                <h3 v-if="position == 1">{{ position }}st place</h3>
+                <h3 v-else-if="position == 2">{{ position }}nd place</h3>
+                <h3 v-else-if="position == 3">{{ position }}rd place</h3>
+            </div>
+
+            <div class="color">
+                <h3>Points: {{ song.points }}</h3>
+            </div>
+
+        </div>
+    </div>
+</template>
+<style>
+
+.card:after {
+    content: "Rank";
+    position: absolute;
+    top: 30%;
+    left: -5%;
+    font-size: 9em;
+    font-weight: 800;
+    font-style: italic;
+    color: rgba(255, 255, 255, 0.04);
+
+}
+
+.container .card:nth-child(odd) {
+    transform: translateY(15%);
+}
+
+</style>
+<script>
+export default {
+    name: "RankCard",
+    props: [
+        "song",
+        "position"
+    ]
+}
+</script>
--- src/components/pages/Ranking.vue
+++ src/components/pages/Ranking.vue
@@ -9,18 +9,93 @@
                 <li @click="goToPage('ranking')">Ranking</li>
             </ul>
         </nav>
-        
+        <div class="center">
+            <h1><u>Ranking</u></h1>
+            <div class="container" style="display:flex; justify-content:space-between">
+                <RankCard :song="songs[1]" :position="2"/>
+                <RankCard :song="songs[0]" :position="1"/>
+                <RankCard :song="songs[2]" :position="3"/>
+            </div>
+            
+
+        </div>
     </div>
 </template>
+<style>
+.center {
+    width: 50%;
+}
 
+</style>
 <script>
+import RankCard from '../RankCard.vue';
 
 export default {
     name: "Rankingpage",
+    components: {
+        RankCard
+
+    },
+    data() {
+        return {
+            songs: []
+        }
+    },
     methods: {
         goToPage(page) {
             this.$emit("change-page", page);
+        },
+        fetchSongs() {
+            const url = "http://webservies.be/eurosong/Songs";
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((songs) => {
+                    let correctSongs = songs.filter(song => song.spotify != "string");
+                    this.fetchVotes(correctSongs);
+                    this.fetchArtists(correctSongs);
+                    
+                });
+        },
+        fetchVotes(songs) {
+            const url = "http://webservies.be/eurosong/Votes";
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((votes) => {
+                    songs.map(song => {
+                       const vote = votes.filter((vote) => vote.songID == song.id);
+                       song.points = 0;
+                       vote.forEach(v => {
+                           if (v.songID == song.id) song.points += v.points
+                       });
+                       return song;
+                    });
+                    let sorted = songs.sort((a, b) => (a.points < b.points ? 1 : -1));
+                    this.songs = sorted;   
+                    
+                });     
+        },
+        fetchArtists(songs) {
+            const url = "http://webservies.be/eurosong/Artists";
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((artists) => {
+                    songs.map(song => {
+                       const artist = artists.find((artist) => artist.id == song.artist);
+                       song.artist = artist;
+                       return song;
+                    });
+                    this.songs = songs;
+                });
         }
+    },
+    mounted() {
+        this.fetchSongs();
     }
 }
 </script>
